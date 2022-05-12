b49ab60bfdf2003211ee0346cfb088af10ab8e2a
sriram95-devops <70420859+sriram95-devops@users.noreply.github.com>
2022-05-12 19:21:08 +0530

    Update webapp-db.tf

--- Terraform/webapp-db.tf
+++ Terraform/webapp-db.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
 resource "azurerm_resource_group" "rg" {
   name     = var.resource-group-name
   location = var.location
